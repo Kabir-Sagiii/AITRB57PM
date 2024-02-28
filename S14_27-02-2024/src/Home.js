d401d349e55c601d519c5a53c92271259535e986
Kabir <sagithedeveloper@gmail.com>
2024-02-28 06:17:33 +0530

    added the code

---
+++ S14_27-02-2024/src/Home.js
@@ -0,0 +1,14 @@
+import { Profile } from "./Profile";
+import { Product } from "./Product";
+function Home() {
+  return (
+    <div>
+      <Product title="Mobile Brand" />
+
+      <Product title="Jewelery's" />
+      <Product title="Electronics and Clothing" />
+    </div>
+  );
+}
+
+export default Home;
