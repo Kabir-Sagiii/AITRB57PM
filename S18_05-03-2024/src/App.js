96117ac1870a08f9d802df2a5b4f34980bd96f18
Kabir <sagithedeveloper@gmail.com>
2024-03-05 19:59:45 +0530

    added the code

---
+++ S18_05-03-2024/src/App.js
@@ -0,0 +1,9 @@
+import Counter from "./components/counter/Counter";
+import State from "./components/state/State";
+import Profile from "./components/profile/Profile";
+
+function App() {
+  return <div className="App">{<Profile />}</div>;
+}
+
+export default App;
