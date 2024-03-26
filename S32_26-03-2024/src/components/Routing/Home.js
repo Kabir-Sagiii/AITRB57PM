7df3ce2e990e1594f92e97c826b826541b721c70
Kabir <sagithedeveloper@gmail.com>
2024-03-26 20:09:30 +0530

    added the code

---
+++ S32_26-03-2024/src/components/Routing/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Component</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero cumque
+        recusandae ipsa, veniam rem modi illum maxime perspiciatis itaque
+        pariatur! Excepturi, laborum ea nisi saepe vel voluptatem quidem! Odio
+        magnam architecto, hic voluptatum, officia fuga a laudantium optio omnis
+        veritatis nulla recusandae autem aliquid ex enim maxime eius consectetur
+        dolorum? Odit ad, atque fugiat consequatur rem ab, laboriosam, soluta ex
+        velit eos tenetur provident numquam in maiores eligendi. Repellendus aut
+        perspiciatis exercitationem tempora aspernatur eos distinctio molestias
+        porro, amet magnam commodi fuga cum expedita eaque ea saepe. Ratione
+        neque quod beatae dolorem temporibus debitis, officiis accusantium
+        architecto, suscipit quibusdam itaque iste, recusandae incidunt
+        explicabo. Pariatur quia quam assumenda deserunt architecto, dignissimos
+        magni tempora natus labore, itaque deleniti enim reprehenderit
+        excepturi, sed quod nesciunt neque accusamus accusantium cum et esse. Ex
+        quia rem obcaecati officia similique reiciendis sequi eos hic nesciunt
+        recusandae, omnis iste. Obcaecati illo omnis, libero illum voluptas ea
+        laboriosam sint dolorum eaque dolor quae magnam minima suscipit iste
+        delectus quidem commodi blanditiis! Voluptates ipsum exercitationem
+        impedit, quas ipsam eius voluptatum tenetur neque rerum expedita,
+        tempore vel velit mollitia eveniet totam et sed architecto dolorem nihil
+        voluptatibus assumenda ducimus magnam. Doloremque repellendus beatae
+        aliquid eum earum at totam quisquam?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ S32_26-03-2024/src/components/nav/Nav.js
@@ -0,0 +1,101 @@
+import { useContext } from "react";
+import AuthContext from "../Login-Logout/AuthContext";
+import { Link } from "react-router-dom";
+function Nav() {
+  let contextData = useContext(AuthContext);
+
+  const logoutUser = () => {
+    contextData.logout();
+  };
+  return (
+    <div style={{ display: "flex", height: "70px", backgroundColor: "black" }}>
+      <div
+        style={{
+          width: "40%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2 style={{ color: "white" }}>React Routing API</h2>
+      </div>
+      <div
+        style={{
+          width: "60%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Link
+          to="/"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            marginRight: "50px",
+          }}
+        >
+          Home
+        </Link>
+        <Link
+          to="/github"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            marginRight: "50px",
+          }}
+        >
+          Github
+        </Link>
+        <Link
+          to="/reducer"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            marginRight: "50px",
+          }}
+        >
+          useReducer
+        </Link>
+        <Link
+          to="/profile"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            marginRight: "50px",
+          }}
+        >
+          Profile
+        </Link>
+        <Link
+          to="/products"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            marginRight: "50px",
+          }}
+        >
+          Products
+        </Link>
+        <Link
+          to="/cart"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            marginRight: "50px",
+          }}
+        >
+          Cart-Details
+        </Link>
+        <button
+          style={{ border: "none", padding: "7px 15px" }}
+          onClick={logoutUser}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
